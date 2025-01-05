62341ee209277b2df61e67ae256bbecc63dbd56d
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-01-05 11:15:18 +0900

    wireframe, transparent - opacity 종속적. BasicMaterial 3가지 속성 확인

--- src/js/index.js
+++ src/js/index.js
@@ -28,7 +28,10 @@ scene.add(light);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-    color: 0xffaaaa
+    color: 0xffaaaa,
+    // wireframe: true,
+    transparent: true,
+    opacity: 0.5,
 });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
