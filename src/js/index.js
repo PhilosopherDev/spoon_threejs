00a19f702647bc8c75ba849a537129562e70ab5c
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-01-05 19:35:48 +0900

    한라봉 모양 제작

--- src/js/index.js
+++ src/js/index.js
@@ -27,31 +27,35 @@ const light = new THREE.DirectionalLight(0xffffff);
 light.position.set(2, 4, 3);
 scene.add(light);
 
-const geometry = new THREE.DodecahedronGeometry(1);
-const material = new THREE.MeshStandardMaterial({
-    color: 0xffaaaa,
-});
+// 한라봉
+const bodyMaterial = new THREE.MeshStandardMaterial({
+    color: 0xff7f00,
+    // wireframe: true
+})
+const bottomGeometry = new THREE.DodecahedronGeometry(2, 1);
+const bottom = new THREE.Mesh(bottomGeometry, bodyMaterial);
+scene.add(bottom);
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const topGeometry = new THREE.TetrahedronGeometry(0.8, 3);
+const top = new THREE.Mesh(topGeometry, bodyMaterial);
+top.position.y = 1.7;
+scene.add(top)
 
-// 1. 위치
-// mesh.position.x = 1;
-// mesh.position.y = 2;
-mesh.position.set(0, 2, 1);
+const leafMaterial = new THREE.MeshStandardMaterial({
+    color: 0x008000,
+    side: THREE.DoubleSide,
+})
 
-// 2. 회전
-mesh.rotation.y = THREE.MathUtils.degToRad(30);
+const stemGeometry = new THREE.CylinderGeometry(0.1, 0.1, 0.4);
+const stem = new THREE.Mesh(stemGeometry, leafMaterial);
+stem.position.y = 2.5;
+scene.add(stem);
 
-// 3. 크기
-mesh.scale.x = 1.2
-mesh.scale.y = 1.2
-
-
-// axesHelper
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
+const leafGeometry = new THREE.SphereGeometry(0.5, 32, 16, 0, Math.PI / 3);
+const leaf = new THREE.Mesh(leafGeometry, leafMaterial);
+leaf.position.set(-0.5, 2.4, -0.1);
+leaf.rotation.z = Math.PI / -2;
+scene.add(leaf);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
