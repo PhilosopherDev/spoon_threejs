98b48d5f362f63b3d62ec80b242a6faa7374fbab
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-01-07 22:15:32 +0900

    renderer.shadowMap, castShadow, receiveShadow 적용

--- src/js/shadow.js
+++ src/js/shadow.js
@@ -15,13 +15,15 @@ const renderer = new THREE.WebGLRenderer({
     canvas: $result
 });
 renderer.setSize($result.clientWidth, $result.clientHeight);
-
+renderer.shadowMap.enabled = true;
 
 // 도형
 const geometry = new THREE.SphereGeometry(1);
 const material = new THREE.MeshStandardMaterial({ color: 0x2E6FF2 })
 const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true; // 그림자를 만들 mesh에는 cast
 scene.add(cube);
+
 
 const geometry2 = new THREE.PlaneGeometry(10, 10);
 const material2 = new THREE.MeshStandardMaterial({
@@ -31,15 +33,15 @@ const material2 = new THREE.MeshStandardMaterial({
 const plane = new THREE.Mesh(geometry2, material2);
 plane.rotation.x = Math.PI / -2;
 plane.position.y = -1;
+plane.receiveShadow = true; // 그림자가 맺히는 mesh
 scene.add(plane);
 
 // 빛 - DirectionalLight, PointLight, SpotLight 3가지가 그림자 생김
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0, 2, 2);
-scene.add(directionalLight);
+directionalLight.castShadow = true; // 빛에도 적용
 
-const dlHelper = new THREE.DirectionalLightHelper(directionalLight, 1, 0xff0000);
-scene.add(dlHelper);
+scene.add(directionalLight);
 
 
 // OrbitContorls
